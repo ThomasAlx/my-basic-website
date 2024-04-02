7f0657d5e26695b31267ce977a54c6dc2bed4d0d
thomas <thomas.el.alex@gmail.com>
2024-04-02 07:59:10 +0200

    adds routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import About from './components/About';
+import Home from './components/Home';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <nav>
-        <ul id="navigation">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav>
+          <ul id="navigation">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <p>Hello!</p>
+        <Routes>
+          <Route path="/" element={<Home/>}>
+            {/* <Home /> */}
+          </Route>
+          <Route path="/" element={<about/>}>
+            {/* <About /> */}
+          </Route>
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import '../App.css';
+import React from 'react';
+import logo from '../logo.svg';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <p>This is the Home page</p>
+            </div>
+            
+        );
+    }
+
+}
+
+export default Home;
